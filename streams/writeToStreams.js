a5832074e1e828c3d73e875d34f2d4fdd8be84b6
aremesh <aremesh@gmail.com>
2017-04-28 09:47:37 -0400

    The Write to stream file has been completed..

---
+++ streams/writeToStreams.js
@@ -0,0 +1,16 @@
+var fs = require("fs");
+var dataToWrite = "Here is the sample data to write on the table";
+//create a writable stream
+var writeStream = fs.createWriteStream("output.txt");
+//write the data to stream with encoding to be utf-8
+writeStream.write(dataToWrite, 'UTF8');
+//mark the end of the file
+writeStream.end();
+//handle stream events-->finish, and error
+writeStream.on('finish', function () {
+    console.log("write completed.");
+});
+writeStream.on('error', function (err) {
+    console.log(err.stack);
+});
+console.log("Program ended");
